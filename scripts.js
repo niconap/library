314396ffb438d7ccc5a6c283f5778d02c05ef1f9
niconap <nico.nap@outlook.com>
2020-07-24 13:49:16 +0200

    Make pop-up form.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,64 @@
 let myLibrary = [];
+let form = false;
 
 // Select elements on the page
-const lib = document.getElementById("lib");
-const ttl = document.getElementById("title");
-const ath = document.getElementById("author");
-const rd = document.getElementById("read");
-const add = document.getElementById("add");
+const addbook = document.getElementById("addbook");
+
 
 // Add eventlistener to the button
-add.addEventListener("click", addBookToLibrary);
+addbook.addEventListener("click", popUpForm);
+// add.addEventListener("click", addBookToLibrary);
+
+// Make the form pop up
+function popUpForm() {
+    if (form == false){
+        let input = document.getElementById("input")
+        let title = document.createElement("input");
+        title.setAttribute("type", "text");
+        title.setAttribute("id", "title")
+        input.appendChild(title);
+        let author = document.createElement("input");
+        author.setAttribute("type", "text");
+        author.setAttribute("id", "author")
+        input.appendChild(author);
+        let read = document.createElement("input");
+        read.setAttribute("type", "checkbox");
+        read.setAttribute("id", "read")
+        read.innerHTML = "Read?";
+        input.appendChild(read);
+        let par = document.createElement("p");
+        par.innerHTML = "Read?";
+        input.appendChild(par);
+        let add = document.createElement("button");
+        add.setAttribute("id", "add")
+        add.innerHTML = "Add"
+        input.appendChild(add);
+        const lib = document.getElementById("lib");
+        const ttl = document.getElementById("title");
+        const ath = document.getElementById("author");
+        const rd = document.getElementById("read");
+        const ad = document.getElementById("add");
+        ad.addEventListener("click", function(){
+            addBookToLibrary(ttl.value, ath.value, rd.checked);
+        });
+        form = true;
+    } else {
+        return;
+    }
+}
+
+// Delete the form
+function deleteForm () {
+    let title = document.getElementById("title");
+    title.parentNode.removeChild(title);
+    let author = document.getElementById("author");
+    author.parentNode.removeChild(author);
+    let read = document.getElementById("read");
+    read.parentNode.removeChild(read);
+    let add = document.getElementById("add");
+    add.parentNode.removeChild(add);
+    form = false;
+}
 
 // Construct book
 function Book(title, author, read) {
@@ -18,19 +68,18 @@ function Book(title, author, read) {
 }
 
 // Make a new book and render on the page
-function addBookToLibrary() {
-    myLibrary.push(new Book(ttl.value, ath.value, rd.checked));
-    if (ttl.value == "" || ath.value == "") {
+function addBookToLibrary(title, author, read) {
+    myLibrary.push(new Book(title, author, read));
+    if (title == "" || author == "") {
         alert("Please enter a title and an author!");
     } else {
-        ttl.value = "";
-        ath.value = "";
-        rd.checked = false;
+        deleteForm();
         render(myLibrary);
     }
 }
 
 // Append elements to show all books on the page
+// and add a button to delete the book
 function render(array) {
     lib.innerHTML = "";
     array.forEach(book => {
